14cf4647bd5b4f142223cc486fa97ca0cf43bf19
mofa8377 <mofa8377@gmail.com>
2025-10-11 05:08:39 +0000

    feat: add explicit check for supabase env variables

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+const supabaseUrl = import.meta.env.VITE_URL;
+const supabaseAnonKey = import.meta.env.VITE_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and anonymous key are required.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
